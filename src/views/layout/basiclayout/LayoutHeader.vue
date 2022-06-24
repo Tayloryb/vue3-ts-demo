089ad37bb90818a92c55d8e18f1666fe7bb737c3
taylory_zyb <taylory_zyb@163.com>
2022-06-24 18:03:44 +0800

    公共样式

--- src/views/layout/basiclayout/LayoutHeader.vue
+++ src/views/layout/basiclayout/LayoutHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="layout-header pd-20">this is header</div>
+  <div class="layout-header pl-20 p4-20 fw-500 fs-20">this is header</div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
